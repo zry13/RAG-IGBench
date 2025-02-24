8282972d3b84f8d4557a2a95aae84f7fd5270c97
zry13 <129248065+zry13@users.noreply.github.com>
2025-02-24 12:29:16 +0800

    Update scores.py

--- score/scores.py
+++ score/scores.py
@@ -13,7 +13,7 @@ def main(args):
         total_rouge1 += d['rouge1_score']
         total_ed += d['edit_distance']
         total_kendall += d['kendall_score']
-        total_align += d['alignmen_score']
+        total_align += d['alignment_score']
         total_clip += d['clip_score']
     num = len(datas)
     print(f'----------eval result of {num} valid cases-----------')
@@ -26,7 +26,7 @@ def main(args):
 
 def parse_arguments():
     parse = argparse.ArgumentParser(description='Generation setting of gpt4o.')
-    parse.add_argument('input_file', type=str, help='The evaluation file.')
+    parse.add_argument('--input_file', type=str, help='The evaluation file.')
 
     args = parse.parse_args()
 
